71451b4b6f3afaf43ed25becc9b0a4c02afb819b
KenEkm <kenankeni@web.de>
2022-12-01 18:10:42 +0100

    Added interactive icons and toggle functionality

--- JS im Browser Teil 2/src/main.js
+++ JS im Browser Teil 2/src/main.js
@@ -6,10 +6,26 @@ document.addEventListener("DOMContentLoaded", () =>{
     const cardHeaders = document.getElementsByClassName("card-header")
 
     for (const x of cardHeaders){
+        const cardBody = x.nextElementSibling
+        const cardIcon = x.querySelector(".card-toggle-icon")
         console.log("Card-Headers: ", x)
 
-        x.addEventListener("click", () => {
-            alert("Card header clicked.")
+        //Initiales Einklappen der Karten
+        cardBody.classList.add("d-none")
+
+        //toggle funktion für card bodies mit d-none von bootstrap
+        x.addEventListener("click", (event) => {
+
+            cardBody.classList.toggle("d-none")
+
+            if(cardBody.classList.contains("d-none")){
+                cardIcon.classList.remove("fa-angle-double-down")
+                cardIcon.classList.add("fa-angle-double-right")
+            } else {
+                cardIcon.classList.remove("fa-angle-double-right")
+                cardIcon.classList.add("fa-angle-double-down")
+            }
+
         })
     }
 
