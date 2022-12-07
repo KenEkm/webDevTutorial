3bb2c4b9562f4b9f3d7c1cce32add1c7b8212c0b
KenEkm <kenankeni@web.de>
2022-12-07 23:01:01 +0100

    new template, adding tasks

--- Project ToDo List/src/main.js
+++ Project ToDo List/src/main.js
@@ -2,6 +2,37 @@
 
 document.addEventListener("DOMContentLoaded", () =>{
 
+    const newTodoElement = document.querySelector(".new-todo")
+    const todoListElement = document.querySelector(".todo-list")
     
-    
+    newTodoElement.addEventListener("keypress", (event) => {
+        if(event.key === "Enter" && newTodoElement.value !== ""){
+            const newButtonElement = document.createElement("button")
+            newButtonElement.classList.add("destroy")            
+
+
+            const newLabelElement = document.createElement("label")
+            newLabelElement.appendChild(document.createTextNode(newTodoElement.value))
+
+            const newInputCheckbox = document.createElement("input")
+            newInputCheckbox.type = "checkbox"
+            newInputCheckbox.classList.add("toggle")
+
+            const newDivElement = document.createElement("div")
+            newDivElement.classList.add("view")
+            newDivElement.appendChild(newInputCheckbox)
+            newDivElement.appendChild(newLabelElement)
+            newDivElement.appendChild(newButtonElement)
+
+            const newLiElement = document.createElement("li")
+            newLiElement.appendChild(newDivElement)
+
+            todoListElement.appendChild(newLiElement)
+
+            newTodoElement.value = ""
+        }
+    })
+
+
+
 })
