19249611a7d94f8fe0f09556ababd4e92776f114
KenEkm <kenankeni@web.de>
2023-01-31 18:19:38 +0100

    accessing one specific product with the api call

--- Nutrition API Project/index.js
+++ Nutrition API Project/index.js
@@ -2,16 +2,25 @@
 
 
 const axios = require("axios")
-
+/*
 axios.get("https://api.nal.usda.gov/fdc/v1/foods/search", {
-        params: {
-            api_key: "zLXDRhUpG9vmq2gToKhTQOTq2kFdV44khbgwayXo",
-            query: "Apple"
-        }
-    })
-    .then(function(response){
-        for(const food of response.data['foods']){
+    params: {
+        api_key: "zLXDRhUpG9vmq2gToKhTQOTq2kFdV44khbgwayXo",
+        query: "Apple"
+    }
+})
+    .then(function (response) {
+        for (const food of response.data['foods']) {
             console.log(food['fdcId'] + ": " + food['description'])
         }
     })
+*/
 
+axios.get("https://api.nal.usda.gov/fdc/v1/food/454004", {
+    params: {
+        api_key: "zLXDRhUpG9vmq2gToKhTQOTq2kFdV44khbgwayXo"
+    }
+})
+    .then(function (response) {
+        console.log(response.data)
+    })
