884c180b70530ba77776e9c9de5a826bb7458d18
KenEkm <kenankeni@web.de>
2023-02-06 21:11:18 +0100

    implemented product list

--- Nutrition API Project/src/controllers/productList.js
+++ Nutrition API Project/src/controllers/productList.js
@@ -1,5 +1,8 @@
 "use strict"
 
+const{info} = require("../api/product")
+const addProductTemplate = require("../templates/ProductList/addProduct.ejs")
+
 /**
  * 
  * @param {HTMLTableElement} listElement 
@@ -14,7 +17,14 @@ productList.prototype.init = function(){
 }
 
 productList.prototype.addProduct = function(fdcId){
-    console.log("fdcId: " + fdcId)
+    info(fdcId)
+        .then((product) => {
+            const productHtml = addProductTemplate({title: product['description']})
+            
+            //this.listElement.innerHTML = productHtml |diese Schreibweise lädt alle
+            //tr elemente neu und ist daher langsam. Nachfolgender Code lädt nur das Neue.
+            this.listElement.insertAdjacentHTML("beforeend", productHtml)
+        })
 }
 
 module.exports = productList
