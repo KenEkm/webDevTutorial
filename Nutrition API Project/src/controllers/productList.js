1940c84b35dad028717cb445a5fba4cb2fdea4ad
KenEkm <kenankeni@web.de>
2023-02-07 18:53:48 +0100

    calculating nutrients for amount

--- Nutrition API Project/src/controllers/productList.js
+++ Nutrition API Project/src/controllers/productList.js
@@ -19,12 +19,66 @@ productList.prototype.init = function(){
         const fdcId = parseInt(event.handleObj.getAttribute("data-fdc"), 10)
         this.removeProduct(fdcId)
     })
+    on(".product-search__amount", "change", (event) => {
+        const fdcId = parseInt(event.handleObj.getAttribute("data-fdc"), 10)
+        const value = parseInt(event.handleObj.value, 10)
+        this.updateAmount(fdcId, value)
+    })
+}
+
+productList.prototype.getNutrientsForProduct = function (product){
+    const nutrients = {
+        carbs: 0,
+        protein: 0,
+        fat: 0
+    }
+    for(const foodNutrient of product.product.foodNutrients){
+        if(foodNutrient.nutrientNumber === "205"){
+            nutrients.carbs = foodNutrient.value
+        } else if(foodNutrient.nutrientNumber === "204"){
+            nutrients.fat = foodNutrient.value
+        } else if(foodNutrient.nutrientNumber === "203"){
+            nutrients.protein = foodNutrient.value
+        }
+    }
+    return {
+        carbs: (nutrients.carbs / 100) * product.amount,
+        protein: (nutrients.protein / 100) * product.amount,
+        fat: (nutrients.fat / 100) * product.amount
+    }
+}
+
+productList.prototype.getNutrients = function(){
+    const nutrients = {
+        carbs: 0,
+        protein: 0,
+        fat: 0
+    }
+
+    for(const product of this.products){
+        const productNutrients = this.getNutrientsForProduct(product)
+
+        nutrients.carbs += productNutrients.carbs
+        nutrients.protein += productNutrients.protein
+        nutrients.fat += productNutrients.fat
+    }
+    return nutrients
+}
+
+productList.prototype.updateAmount = function(fdcId, value){
+    for(const product of this.products){
+        if(product.product['fdcId'] === fdcId){
+            product.amount = value
+            break
+        }
+    }
+    console.log("updateAmount: " + value)
 }
 
 productList.prototype.removeProduct = function(fdcId){
     for(const i in this.products){
         const product = this.products[i]
-        if(product['fdcId'] === fdcId){
+        if(product.product['fdcId'] === fdcId){
             this.products.splice(i, 1)
             const trElement = document.querySelector(".product-search__product-row[data-fdc='" + fdcId + "']")
             if(trElement) trElement.remove()
@@ -37,7 +91,10 @@ productList.prototype.removeProduct = function(fdcId){
 productList.prototype.addProduct = function(fdcId){
     info(fdcId)
         .then((product) => {
-            this.products.push(product)
+            this.products.push({
+                amount:100,
+                product: product
+            })
             const productHtml = addProductTemplate({
                 title: product['description'],
                 fdcId: product['fdcId']
@@ -46,6 +103,8 @@ productList.prototype.addProduct = function(fdcId){
             //this.listElement.innerHTML = productHtml |diese Schreibweise lädt alle
             //tr elemente neu und ist daher langsam. Nachfolgender Code lädt nur das Neue.
             this.listElement.insertAdjacentHTML("beforeend", productHtml)
+
+            //this.getNutrients()
         })
 }
 
