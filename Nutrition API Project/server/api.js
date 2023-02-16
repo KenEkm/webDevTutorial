9dc96712718fae6ae4d403010ee074b654aac78b
KenEkm <kenankeni@web.de>
2023-02-16 16:05:05 +0100

    passing requests and response of api through local nodejs server

---
+++ Nutrition API Project/server/api.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const axios = require("axios")
+
+const auth = {
+  username: "zLXDRhUpG9vmq2gToKhTQOTq2kFdV44khbgwayXo"
+}
+
+module.exports.search = function search(term) {
+  return axios
+    .post("https://api.nal.usda.gov/fdc/v1/search", {
+      generalSearchInput: term
+    }, {auth})
+    .then((response) => response.data)
+}
+
+module.exports.info = function info(fdcId) {
+  return axios
+    .get("https://api.nal.usda.gov/fdc/v1/" + fdcId, {auth})
+    .then((response) => response.data)
+}
--- Nutrition API Project/server/app.js
+++ Nutrition API Project/server/app.js
@@ -2,9 +2,10 @@
 //Node Server mit http Modul
 const http = require("http")
 const url = require("url")
-
+const querystring = require("querystring")
 const servePublic = require("./public")
 
+const {search, info} = require("./api")
 
 const app = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url)
@@ -12,6 +13,56 @@ const app = http.createServer((req, res) => {
     if(parsedUrl.pathname.substr(0, 8) === "/public/"){
         return servePublic(parsedUrl, res)
     }
+    if(parsedUrl.pathname.substr(0,5) === "/api/"){
+        if(parsedUrl.pathname === "/api/search"){
+            const parsedQuery = querystring.parse(parsedUrl.query)
+            if("q" in parsedQuery){
+                const q = "" + parsedQuery["q"]
+                return search(q)
+                    .then((data) => {
+                        res.writeHead("200", {"Content-Type": "application/json"})
+                        res.write(JSON.stringify(data))
+                        res.end()
+                    })
+                    .catch((err) => {
+                        res.writeHead("500", {"Content-Type": "text/plain"})
+                        res.write("Errorcode 500 Internal Server Error")
+                        res.end()
+                    })
+            } else {
+                res.writeHead("422", {"Content-Type": "text/plain"})
+                res.write("Errorcode 422 Missing Parameter")
+                res.end()
+                return
+            }
+        }
+        if(parsedUrl.pathname === "/api/info"){
+            const parsedQuery = querystring.parse(parsedUrl.query)
+            if("q" in parsedQuery){
+                const q = "" + parsedQuery["q"]
+                return info(q)
+                    .then((data) => {
+                        res.writeHead("200", {"Content-Type": "application/json"})
+                        res.write(JSON.stringify(data))
+                        res.end()
+                    })
+                    .catch((err) => {
+                        res.writeHead("500", {"Content-Type": "text/plain"})
+                        res.write("Errorcode 500 Internal Server Error")
+                        res.end()
+                    })
+            } else {
+                res.writeHead("422", {"Content-Type": "text/plain"})
+                res.write("Errorcode 422 Missing Parameter")
+                res.end()
+                return
+            }
+        }
+        return
+    }
+    res.writeHead("404", {"Content-Type": "text/plain"})
+    res.write("Errorcode 404 File Not Found")
+    res.end()
 })
 
 app.listen(8081)
