e79008bd018cef8210f1c6ef462cb6b878168091
KenEkm <kenankeni@web.de>
2023-02-16 16:53:27 +0100

    finalizing nutrition api project

--- Nutrition API Project/server/app.js
+++ Nutrition API Project/server/app.js
@@ -2,6 +2,8 @@
 //Node Server mit http Modul
 const http = require("http")
 const url = require("url")
+const fs = require("fs")
+const path = require("path")
 const querystring = require("querystring")
 const servePublic = require("./public")
 
@@ -9,6 +11,21 @@ const {search, info} = require("./api")
 
 const app = http.createServer((req, res) => {
     try{
+        if(req.url === "/"){
+            fs.readFile(path.join(__dirname, "..", "public", "index.html"), (err, content) => {
+                if(err){
+                    res.writeHead(500, {"Content-Type": "text/plain"})
+                    res.write("Errorcode 500 Internal Server Error")
+                    res.end()
+                    return
+                }
+                res.writeHead(200, {"Content-Type": "test/html"})
+                res.write(content)
+                res.end()
+            })
+            return
+        }
+
         const parsedUrl = url.parse(req.url)
 
     if(parsedUrl.pathname.substr(0, 8) === "/public/"){
@@ -21,17 +38,17 @@ const app = http.createServer((req, res) => {
                 const q = "" + parsedQuery["q"]
                 return search(q)
                     .then((data) => {
-                        res.writeHead("200", {"Content-Type": "application/json"})
+                        res.writeHead(200, {"Content-Type": "application/json"})
                         res.write(JSON.stringify(data))
                         res.end()
                     })
                     .catch((err) => {
-                        res.writeHead("500", {"Content-Type": "text/plain"})
+                        res.writeHead(500, {"Content-Type": "text/plain"})
                         res.write("Errorcode 500 Internal Server Error")
                         res.end()
                     })
             } else {
-                res.writeHead("422", {"Content-Type": "text/plain"})
+                res.writeHead(422, {"Content-Type": "text/plain"})
                 res.write("Errorcode 422 Missing Parameter")
                 res.end()
                 return
@@ -43,17 +60,17 @@ const app = http.createServer((req, res) => {
                 const q = "" + parsedQuery["q"]
                 return info(q)
                     .then((data) => {
-                        res.writeHead("200", {"Content-Type": "application/json"})
+                        res.writeHead(200, {"Content-Type": "application/json"})
                         res.write(JSON.stringify(data))
                         res.end()
                     })
                     .catch((err) => {
-                        res.writeHead("500", {"Content-Type": "text/plain"})
+                        res.writeHead(500, {"Content-Type": "text/plain"})
                         res.write("Errorcode 500 Internal Server Error")
                         res.end()
                     })
             } else {
-                res.writeHead("422", {"Content-Type": "text/plain"})
+                res.writeHead(422, {"Content-Type": "text/plain"})
                 res.write("Errorcode 422 Missing Parameter")
                 res.end()
                 return
@@ -61,12 +78,12 @@ const app = http.createServer((req, res) => {
         }
         return
     }
-    res.writeHead("404", {"Content-Type": "text/plain"})
+    res.writeHead(404, {"Content-Type": "text/plain"})
     res.write("Errorcode 404 File Not Found")
     res.end()
     }
     catch (err){
-        res.writeHead("500", {"Content-Type": "text/plain"})
+        res.writeHead(500, {"Content-Type": "text/plain"})
         res.write("Errorcode 500 Internal Server Error")
         res.end()
     }
